8d484babf59edb5f87811391de32663882e1fa0c
ShreyanshShri <62863886+ShreyanshShri@users.noreply.github.com>
2020-07-26 14:23:20 +0530

    Added language filter

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -39,6 +39,47 @@ router.get('/', (req, mainRes)=>{
                     // console.log(thisInfo)
                     allInfo.push(thisInfo)
                 })
+                // console.log(allInfo)
+                mainRes.json(allInfo)
+            }
+        }
+    })
+})
+
+router.get('/:lang', (req, mainRes)=>{
+    let random = Math.floor(Math.random()*myUserAgents.length);
+    var customHeaderRequest = request.defaults({
+        headers: {
+            'User-Agent': myUserAgents[random]
+          }
+    })
+    const lang = req.params.lang
+
+    customHeaderRequest.get(`https://www.sololearn.com/Leaderboard/${lang}`, (err, res, html)=>{
+        if(!err){
+            
+            const $ = cheerio.load(html)
+            const check = $('.content h1').text()
+            if(check=='Page Not Found' || check=='Error.'){
+                mainres.json({Error:"Maybe Provided Id Is Incorrect..:("});
+                console.log('yuppp')
+            } else{
+                // console.log(html)
+                let allInfo = []
+                $('.item').each((index, ele)=>{
+                    let username = $(ele).find('.name').text().trim()
+                    let XPs = $(ele).find('.points').text().trim()
+                    let rank = $(ele).find('.rank').text().trim()
+                    console.log(username,XPs,rank)
+                    let thisInfo = {
+                        username: username,
+                        xp: XPs,
+                        rank: rank,
+                        lang: lang
+                    }
+                    // console.log(thisInfo)
+                    allInfo.push(thisInfo)
+                })
                 console.log(allInfo)
                 mainRes.json(allInfo)
             }
